e24dd0969e2892fa9f68bb2bb0c45d24e80647a2
Marc McIntosh <marcmcintsoh1987@gmail.com>
2019-03-10 21:02:23 +0100

    Works as well as the exapmle project

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,10 @@ FROM archlinux/base:latest
 
 WORKDIR devpkg
 
-RUN pacman -Sy --noconfirm gcc glibc make tar git curl apr apr-util
+RUN pacman -Sy --noconfirm base-devel tar git curl apr apr-util
 
 COPY . .
 
-RUN make clean && make && make install && ./test.sh
+RUN make clean && make && make install
+RUN devpkg -L
+RUN ./test.sh
--- test.sh
+++ test.sh
@@ -2,32 +2,35 @@ set -e
 
 rm -rf /usr/local/.devpkg
 
-# set-up
+echo "Testing setup devpkg -S";
 ./devpkg -S
 
-# regular tar.gz
-./devpkg -I http://www-us.apache.org/dist/apr/apr-1.6.5.tar.gz
- 
-# regular tar.bz2
-# ./devpkg -I http://mongrel2.org/static/downloads/mongrel2-1.7.5.tar.bz2
- 
-# regular .git
-# ./devpkg -I https://github.com/zedshaw/srpmin.git
- 
-# manually fetch/build if already installed
-# ./devpkg -F https://github.com/zedshaw/srpmin.git
- 
-# ./devpkg -B https://github.com/zedshaw/srpmin.git
- 
-# manually fetch/build it not installed
-# ./devpkg -F http://www-us.apache.org/dist/apr/apr-util-1.6.1.tar.gz
- 
-# ./devpkg -B http://www-us.apache.org/dist/apr/apr-util-1.6.1.tar.gz -c "--with-apr=/usr/local/apr"
- 
-# idempotent build test
+echo "Testing a regular tar.gz";
+./devpkg -I http://mirror.its.uidaho.edu/pub/apache//apr/apr-1.4.5.tar.gz
 
-# ./devpkg -I http://www-us.apache.org/dist/apr/apr-util-1.6.1.tar.gz -c "--with-apr=/usr/local/apr"
+echo "Testing a regular tar.bz2"
+./devpkg -I http://mongrel2.org/static/downloads/mongrel2-1.7.5.tar.bz2
 
-# just run the depends, nothing should build
-# ./devpkg -I ./DEPENDS
+echo "Testing a regular .git"
+./devpkg -I https://github.com/zedshaw/srpmin.git
+
+echo "Test manually fetch/build if already installed"
+./devpkg -F https://github.com/zedshaw/srpmin.git
+
+./devpkg -B https://github.com/zedshaw/srpmin.git
+
+echo "Test manually fetch/build it not installed"
+./devpkg -F http://mirror.its.uidaho.edu/pub/apache//apr/apr-util-1.3.12.tar.gz
+
+./devpkg -B http://mirror.its.uidaho.edu/pub/apache//apr/apr-util-1.3.12.tar.gz -c "--with-apr=/usr/local/apr"
+
+echo "Test idempotent build test"
+./devpkg -I http://mirror.its.uidaho.edu/pub/apache//apr/apr-util-1.3.12.tar.gz -c "--with-apr=/usr/local/apr"
+
+
+echo "Test run the depends, nothing should build"
+./devpkg -I ./DEPENDS
+
+echo "list out what's installed"
+./devpkg -L
 
